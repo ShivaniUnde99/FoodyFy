4fa2c0ebda4ee393faf26d5951d53adbb3269396
ShivaniUnde99 <shivaniunde38@gmail.com>
2026-05-03 15:13:19 +0530

    fix cors + db connection

--- backend/controllers/order.controllers.js
+++ backend/controllers/order.controllers.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv"
 import { count } from "console"
 
 dotenv.config()
-let instance = new RazorPay({
+let instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
